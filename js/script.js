b3a219359b11079d8aaa8dfb3d12f51e99ff26a1
Tirosh Menuka <45517431+menuka1@users.noreply.github.com>
2022-09-08 15:45:18 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+const actualBtn = document.getElementById('actual-btn');
+
+const fileChosen = document.getElementById('file-chosen');
+
+actualBtn.addEventListener('change', function(){
+  fileChosen.textContent = this.files[0].name
+})
